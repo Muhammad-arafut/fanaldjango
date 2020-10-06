3c8c5e3beb6578f6c66886d592802d0781838fba
Fut <61011412@kmitl.ac.th>
2020-10-07 06:38:34 +0700

    final project

--- stock/urls.py
+++ stock/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('about',views.about, name='about'),
+    path('activity',views.activity, name='activity'),
     path('contact',views.contact, name='contact'),
+    path('sign_up',views.sign_up, name='sign_up'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -3,11 +3,17 @@ from .models import Product
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    return render (request, 'frontend/index.html', {'products': products})
+
+    return render (request, 'frontend/index.html')
 
 def about(request):
     return render (request, 'frontend/about.html')
 
+def activity(request):
+    return render (request, 'frontend/activity.html')
+
 def contact(request):
     return render (request, 'frontend/contact.html')
+
+def sign_up(request):
+    return render (request, 'frontend/sign_up.html')
